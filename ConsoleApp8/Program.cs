bf5297fc346a5457fd59410b5c0b786171d2c912
Jan <Jan@DESKTOP-B91JR0P>
2022-03-13 13:52:19 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int cislo = 32;
+int cislo2 = 58;
+if (cislo < cislo2);
+{
+    Console.WriteLine("číslo 32 je menší než číslo 58");
+}
+
